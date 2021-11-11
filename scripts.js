36c849557ba71c8fa20720a5c6be0fabe8c47323
Pat Kelly <PatK80@gmail.com>
2021-11-11 18:53:58 -0500

    Adding functionality for expand toggle

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-// alert("CONNECTED");
+const expandIcon = document.querySelector(".expand-icon");
+const hiddenText = document.querySelector(".president-component-expand-portion-hidden-text");
+
+expandIcon.addEventListener('click', event => {
+    hiddenText.classList.toggle("president-component-expand-portion-hidden-text");
+    // expandIcon.innerText = "X";
+});
