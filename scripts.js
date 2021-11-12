ba5bc95c711a676ac9d27e94f70bdc0f7a586511
Pat Kelly <PatK80@gmail.com>
2021-11-12 18:32:24 -0500

    Adding icon library

--- scripts.js
+++ scripts.js
@@ -3,5 +3,6 @@ const hiddenText = document.querySelector(".president-component-expand-portion-h
 
 expandIcon.addEventListener('click', event => {
     hiddenText.classList.toggle("president-component-expand-portion-hidden-text");
+    // expandIcon.innerHTML= "<ion-icon name='chevron-up-outline'></ion-icon>" ;
     // expandIcon.innerText = "X";
 });
